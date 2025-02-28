030f68f3035f15b7e271d94f1a6844400ce11b86
Ajinkya Ajay Pundle <ratnadeeppundle@gmail.com>
2025-02-28 17:34:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,3 +6,16 @@
 //   loop: false,
 //   showCursor: false,
 // });
+const bar = document.getElementById("bar");
+const nav = document.getElementById("nav");
+const closed = document.getElementById("closed");
+if(bar){
+    bar.addEventListener("click",()=>{
+         nav.classList.add("active");
+    });
+} ;
+if(closed){
+    closed.addEventListener("click",()=>{
+         nav.classList.remove("active");
+    })
+} 
